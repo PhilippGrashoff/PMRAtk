d0a42f1e7f6e34ab9921a5edd1bc72aafb13dc7f
Philipp Reisigl <outdoor@spame.de>
2019-03-22 20:06:14 +0100

    see if circleci takes that

--- src/Data/Cron/DBBackup.php
+++ src/Data/Cron/DBBackup.php
@@ -15,7 +15,7 @@ class DBBackup extends BaseCronJob {
         $return_var = null;
 
         //create dump
-        exec('mysqldump -u '.DB_USER.' -p\''.DB_PASSWORD.'\' --quick --allow-keywords --add-drop-table --complete-insert --quote-names '.substr(DB_STRING, strrpos(DB_STRING, '=') + 1).' > '.$sql_file, $output, $return_var);
+        exec('mysqldump -h '.substr(DB_STRING, strpos(DB_STRING, '=') + 1, (strpos(DB_STRING, ';') - (strpos(DB_STRING, '=') + 1))).' -u '.DB_USER.' -p\''.DB_PASSWORD.'\' --quick --allow-keywords --add-drop-table --complete-insert --quote-names '.substr(DB_STRING, strrpos(DB_STRING, '=') + 1).' > '.$sql_file, $output, $return_var);
         if($return_var !== 0) {
             throw new \atk4\data\Exception('The DB Backup could not be created (exit code '.$return_var.'): '.implode(PHP_EOL, $output));
         }
