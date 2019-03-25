8c565098db47a66a6a9598500bbfd60b184970a3
Philipp Reisigl <outdoor@spame.de>
2019-03-25 18:57:12 +0100

    ...

--- src/Data/FileLogger.php
+++ src/Data/FileLogger.php
@@ -21,11 +21,22 @@ class FileLogger {
     /*
      *
      */
+    public function emptyLogFile() {
+        if(false === file_put_contents($this->fileName, '')) {
+            throw new \atk4\data\Exception('Could not empty log file: '.$this->fileName. ' in '.__FUNCTION__);
+        }
+    }
+
+
+    /*
+     *
+     */
     public function log(string $level, string $message, array $context = []) {
 
         if(false === file_put_contents(
         $this->fileName,
-        $message. PHP_EOL.'    '.$level.' '.implode(', ', $context).PHP_EOL,
+        //$message. PHP_EOL.'    '.$level.' '.implode(', ', $context).PHP_EOL,
+        $message. PHP_EOL,
         FILE_APPEND)) {
             throw new \atk4\data\Exception('Failed to write Logger info to file: '.$this->fileName. ' in '.__FUNCTION__);
         }
