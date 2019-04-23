7135142ae8c41ec1a4b5d74d003b43527044f68f
Philipp Reisigl <outdoor@spame.de>
2019-04-23 22:57:14 +0200

    FieldChangeMessageTrait

---
+++ src/Data/Traits/FieldChangedMessageTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PMRAtk\Data\Traits;
+
+trait FieldChangedMessageTrait {
+
+    /*
+     * adds a Message to the app that a Field value was changed. This can be used if Data level
+     * automatically updates field value to inform user about it
+     */
+    public function addFieldChangedMessage(string $field_name, $old_value, $new_value, string $class = 'warning') {
+        //nothing changed? no message
+        if($old_value == $new_value) {
+            return;
+        }
+
+        //value special if its a hasOne relation
+        if($this->hasRef($field_name) && $this->getRef($field_name) instanceOf \atk4\data\Reference_One) {
+            $refmodel = $this->refModel($field_name);
+            $old_value = $refmodel->tryLoad($old_value)->get($refmodel->title_field);
+            $new_value = $refmodel->tryLoad($new_value)->get($refmodel->title_field);
+        }
+
+        //date handling
+        if(in_array($this->getElement($field_name)->type, ['date', 'time', 'datetime']) && $old_value instanceOf \DateTimeInterFace) {
+            $old_value = $this->_castDateTimeToString($old_value, $this->getElement($field_name)->type);
+        }
+        if(in_array($this->getElement($field_name)->type, ['date', 'time', 'datetime']) && $new_value instanceOf \DateTimeInterFace) {
+            $new_value = $this->_castDateTimeToString($new_value, $this->getElement($field_name)->type);
+        }
+
+        $this->app->addUserMessage($this->elements[$field_name]->getCaption().' wurde geändert von '.$old_value.' in '.$new_value, $class);
+    }
+
+
+    /*
+     * makes german formatted strings from date, time and datetime fields
+     */
+    protected function _castDateTimeToString(\DateTimeInterFace $value, string $type):string {
+        if($type == 'datetime') {
+            return $value->format('d.m.Y H:i:s');
+        }
+        if($type == 'date') {
+            return $value->format('d.m.Y');
+        }
+        if($type == 'time') {
+            return $value->format('H:i:s');
+        }
+        return '';
+    }
+}
---
+++ tests/phpunit/Data/Traits/FieldChangedMessageTraitTest.php
@@ -0,0 +1,76 @@
+<?php
+
+class FCMTraitTest extends \PMRAtk\tests\phpunit\Data\BaseModelA {
+    use \PMRAtk\Data\Traits\FieldChangedMessageTrait;
+}
+
+class FCMTApp extends \PMRAtk\tests\phpunit\TestApp {
+    use \PMRAtk\View\Traits\UserMessageTrait;
+}
+
+class FieldChangedMessageTraitTest extends \PMRAtk\tests\phpunit\TestCase {
+
+    public $testApp;
+
+    /*
+     * empty userMessages before each test
+     */
+    public function setUp() {
+        parent::setUp();
+        $this->testApp = new \FCMTApp(['admin']);
+    }
+
+
+    /*
+     *
+     */
+    public function testAddMessage() {
+        $m = new FCMTraitTest($this->testApp->db);
+        $m->addFieldChangedMessage('name', 'hansi', 'peter');
+        $this->assertEquals(1, count($this->testApp->userMessages));
+    }
+
+
+    /*
+     *
+     */
+    public function testAddMessageHasOne() {
+        //create 2 records of BaseModelB
+        $b1 = new \PMRAtk\tests\phpunit\Data\BaseModelB($this->testApp->db);
+        $b1->set('name', 'Lala');
+        $b1->save();
+        $b2 = new \PMRAtk\tests\phpunit\Data\BaseModelB($this->testApp->db);
+        $b2->set('name', 'Happa');
+        $b2->save();
+
+        $m = new FCMTraitTest($this->testApp->db);
+        $m->addFieldChangedMessage('BaseModelB_id', $b1->id, $b2->id);
+        $this->assertEquals(1, count($this->testApp->userMessages));
+        //Title field should be used instead of ids
+        $this->assertTrue(strpos($this->testApp->userMessages[0]['message'], 'Lala') !== false);
+        $this->assertTrue(strpos($this->testApp->userMessages[0]['message'], 'Happa') !== false);
+    }
+
+
+    /*
+     *
+     */
+    public function testDateFields() {
+        $m = new FCMTraitTest($this->testApp->db);
+        $m->addFieldChangedMessage('date', new \DateTime(),  (new \DateTime())->modify('+ 1 Day'));
+        $m->addFieldChangedMessage('time', new \DateTime(),  (new \DateTime())->modify('+ 1 Hour'));
+
+        $this->assertEquals(2, count($this->testApp->userMessages));
+    }
+
+
+    /*
+     *
+     */
+    public function testNoValueChangeNoMessage() {
+        $m = new FCMTraitTest($this->testApp->db);
+        $m->addFieldChangedMessage('name', 'ha', 'ha');
+
+        $this->assertEquals(0, count($this->testApp->userMessages));
+    }
+}
