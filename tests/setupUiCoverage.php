ff4ad11c34ebb3b83e7d0fb85c5b8e7836980e24
Philipp Reisigl <outdoor@spame.de>
2019-06-13 23:36:02 +0200

    ....

--- tests/setupUiCoverage.php
+++ tests/setupUiCoverage.php
@@ -33,25 +33,25 @@ foreach(new \DirectoryIterator('.') as $file) {
             }
 
 
-            insertCCCode($file->getFilename().'/'.$subdir->getFilename(), true);
+            insertCCCode($file->getFilename().'/'.$subdir->getFilename(), '../');
         }
 
     }
 }
 
 
-function insertCCCode(string $filename, bool $insubdir = false) {
+function insertCCCode(string $filename, string $pathPrefix = '') {
     $content = file_get_contents($filename);
     $content = str_replace('###CCSTART', '
 $coverage = new \SebastianBergmann\CodeCoverage\CodeCoverage(new \SebastianBergmann\CodeCoverage\Driver\Xdebug());
-$coverage->filter()->addDirectoryToWhitelist(\''.$insubdir ? '../' : ''.'src/View\');
+$coverage->filter()->addDirectoryToWhitelist(\''.$pathPrefix.'src/View\');
 $coverage->start(pathinfo(__FILE__, PATHINFO_FILENAME));
 ', $content);
     $content = str_replace('###CCEND', '
 $app->addHook(\'beforeExit\', function () use($coverage) {
     $coverage->stop(true);
     $writer = new \SebastianBergmann\CodeCoverage\Report\PHP();
-    $writer->process($coverage, \''.$insubdir ? '../' : ''.'tests/coverage/\'.basename($_SERVER[\'SCRIPT_NAME\'], \'.php\').\'-\'.uniqid().\'.cov\');
+    $writer->process($coverage, \''.$pathPrefix.'tests/coverage/\'.basename($_SERVER[\'SCRIPT_NAME\'], \'.php\').\'-\'.uniqid().\'.cov\');
 });
 ', $content);
 
