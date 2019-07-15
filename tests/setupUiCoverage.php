678c5dbc4781b2e992c47c9365e278291ba5d4b0
Philipp Reisigl <outdoor@spame.de>
2019-07-15 17:58:37 +0200

    see if that speeds up tests

--- tests/setupUiCoverage.php
+++ tests/setupUiCoverage.php
@@ -43,6 +43,13 @@ foreach(new \DirectoryIterator('.') as $file) {
 function insertCCCode(string $filename, string $pathPrefix = '') {
     $content = file_get_contents($filename);
     $content = str_replace('###CCSTART', '
+\xdebug_set_filter(
+    \XDEBUG_FILTER_CODE_COVERAGE,
+    \XDEBUG_PATH_WHITELIST,
+    [
+        "'.$pathPrefix.'src/View"
+    ]
+);
 $coverage = new \SebastianBergmann\CodeCoverage\CodeCoverage(new \SebastianBergmann\CodeCoverage\Driver\Xdebug());
 $coverage->filter()->addDirectoryToWhitelist(\''.$pathPrefix.'src/View\');
 $coverage->start(pathinfo(__FILE__, PATHINFO_FILENAME));
