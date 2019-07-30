d90e6a131663ba1ed83fc29a3da28f6a588f3d04
Philipp Reisigl <outdoor@spame.de>
2019-07-30 23:01:57 +0200

    processWhiteListFiles for correct UI coverage

--- tests/setupUiCoverage.php
+++ tests/setupUiCoverage.php
@@ -43,8 +43,8 @@ foreach(new \DirectoryIterator('.') as $file) {
 function insertCCCode(string $filename, string $pathPrefix = '') {
     $content = file_get_contents($filename);
     $content = str_replace('###CCSTART', '
-//xdebug_set_filter(XDEBUG_FILTER_CODE_COVERAGE, XDEBUG_PATH_WHITELIST, [\''.$pathPrefix.'src/View/\']);
 $coverage = new \SebastianBergmann\CodeCoverage\CodeCoverage();
+$coverage->setProcessUncoveredFilesFromWhitelist(true);
 $coverage->filter()->addDirectoryToWhitelist(\''.$pathPrefix.'src/View\');
 $coverage->start(pathinfo(__FILE__, PATHINFO_FILENAME));
 ', $content);
